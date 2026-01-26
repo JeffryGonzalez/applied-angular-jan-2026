da238ec74dde832f7591e467fdfa46d83ada0d86
Jeff Gonzalez <jeff@hypertheory.com>
2026-01-26 15:14:54 -0500

    before break

--- src/app/areas/links/feature-landing/internal/home.ts
+++ src/app/areas/links/feature-landing/internal/home.ts
@@ -5,7 +5,9 @@ import { SectionLayout, SectionLink } from '@ht/shared/ui-common/layouts/section
   selector: 'app-dev-home',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [SectionLayout],
-  template: ` <app-ui-section-layout title="Links" [links]="links()" /> `,
+  template: `
+    <app-ui-section-layout title="Links And Resources For Developers" [links]="links()" />
+  `,
   styles: ``,
 })
 export class Home {
