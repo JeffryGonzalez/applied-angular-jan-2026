54d1510a78d46cb251d5da024e991b5b65df37e2
mdp <michael_d_perkovich@progressive.com>
2026-01-29 21:26:08 +0000

    sorting working

--- src/app/areas/books/feature-list/list-landing/internal/pages/home.ts
+++ src/app/areas/books/feature-list/list-landing/internal/pages/home.ts
@@ -1,6 +1,5 @@
-import { DecimalPipe } from '@angular/common';
 import { httpResource } from '@angular/common/http';
-import { Component, ChangeDetectionStrategy, computed } from '@angular/core';
+import { Component, ChangeDetectionStrategy, computed, signal } from '@angular/core';
 import { PageLayout } from '@ht/shared/ui-common/layouts/page';
 import { Stats } from '../widgets/stats';
 
@@ -18,9 +17,34 @@ export type book = {
   imports: [PageLayout, Stats],
   template: `
     <app-ui-page title="list">
-      <app-stats-bar [bookArray]="booksResource.value() ?? []"></app-stats-bar>
+      <app-stats-bar [bookArray]="booksInOrder() ?? []"></app-stats-bar>
+      <div class="flex flex-row-reverse"></div>
+      <div class = "flex flex-row-reverse">
+        <label class="input">
+          <span class="label">Sort by Publish Date</span>
+          <input type="radio" name="radio-6" class="radio radio-accent" [checked]="sortBy() === 'year'" (click)="setSort('year')"/>
+        </label>
+        <label class="input">
+          <span class="label">Sort by Author</span>
+          <input type="radio" name="radio-6" class="radio radio-accent" [checked]="sortBy() === 'author'" (click)="setSort('author')" />
+        </label>
+        <label class="input">
+          <span class="label">Sort by Title</span>
+          <input type="radio" name="radio-6" class="radio radio-accent" [checked]="sortBy() === 'title'" (click)="setSort('title')" />
+        </label>
+      </div>
+      <div class = "flex flex-row-reverse">
+        <label class="input">
+          <span class="label">Descending</span>
+          <input type="radio" name="radio-7" class="radio radio-accent" [checked]="ascDesc() === 'dsc'" (click)="ascDesc.set('dsc')" />
+        </label>
+        <label class="input">
+          <span class="label">Ascending</span>
+          <input type="radio" name="radio-7" class="radio radio-accent" [checked]="ascDesc() === 'asc'" (click)="ascDesc.set('asc')"/>
+        </label>
+      </div>
       <div class="flex flex-row flex-wrap">
-        @for(book of booksResource.value(); track book.id){
+        @for(book of booksInOrder(); track book.id){
         <div class="card w-96 bg-base-100 card-md shadow-sm m-2">
             <div class="card-body">
               <h2 class="card-title">{{book?.title}}</h2>
@@ -38,4 +62,22 @@ export type book = {
 })
 export class HomePage {
   protected booksResource = httpResource<book[]>(() => '/api/books');
+  protected sortBy = signal<'title' | 'year' | 'author'>('title');
+  protected ascDesc = signal<'asc' | 'dsc'>('asc');
+
+  protected booksInOrder = computed(() => {
+    const multiplier = this.ascDesc() === 'asc' ? 1 : -1;
+    switch(this.sortBy()){
+      case 'title':
+        return this.booksResource?.value()?.sort((a, b) => multiplier * a.title.localeCompare(b.title));
+      case 'year':
+        return this.booksResource?.value()?.sort((a, b) => multiplier * a.year - b.year);
+      case 'author':
+        return this.booksResource?.value()?.sort((a, b) => multiplier * a.author.localeCompare(b.author));
+    }
+  })
+
+  protected setSort(sortType: 'title' | 'year' | 'author'){
+    this.sortBy.set(sortType);
+  }
 }
